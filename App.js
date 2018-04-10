ce2156232df4110767f94d84f585ef973959d451
Darren Oh <darren@oh.name>
2018-04-10 11:14:29 -0400

    feat: Add status bar

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {Constants} from 'expo';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <View  style={{height: Constants.statusBarHeight}}>
+          <StatusBar translucent barStyle='dark-content' />
+        </View>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
